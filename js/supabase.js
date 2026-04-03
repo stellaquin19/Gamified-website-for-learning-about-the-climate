e8e1195b3031a7989dc76aef7f0522efe1a43358
Stella Quintana <156734586+stellaquin19@users.noreply.github.com>
2026-04-02 23:33:19 -0500

    Add files via upload

---
+++ js/supabase.js
@@ -0,0 +1,5 @@
+const { createClient } = window.supabase;
+const supabaseClient = createClient(
+    'https://wvfpfxxdmbhnwmlbaefx.supabase.co',
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind2ZnBmeHhkbWJobndtbGJhZWZ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUxNzUzMTksImV4cCI6MjA5MDc1MTMxOX0.h8ohRScTP_3iy4ZX9RdEcdrjTlbqY_NpIRAD0cjWUAA'
+);
